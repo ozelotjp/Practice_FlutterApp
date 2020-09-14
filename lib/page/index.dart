c7d94e13e586e4b1f801c76bc3852a8bafdd1a01
ozelotjp <contact@ozelot.app>
2020-09-15 00:29:15 +0900

    wip

--- lib/page/index.dart
+++ lib/page/index.dart
@@ -1,39 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class IndexPage extends StatefulWidget {
-  @override
   _IndexPage createState() => _IndexPage();
 }
 
 class _IndexPage extends State<IndexPage> {
-  final list = ['foo', 'bar', 'meow'];
-
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('ShareTodo'),
-      ),
-      body: ListView.builder(
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            child: ListTile(
-              title: Text(list[index]),
-              onTap: () {
-
-              },
-            ),
-          );
-        },
-        itemCount: list.length,
-      ),
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('todo').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError){
+          return new Text('Error: ${snapshot.error}');
+        }
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return new Text('Loading...');
+          default:
+            return new ListView(
+              children: snapshot.data.docs.map((DocumentSnapshot document) {
+                return new ListTile(
+                  title: new Text(document.data()["text"]),
+                );
+              }).toList(),
+            );
+        }
+      },
     );
   }
-}
 
-class _dialog {
-  Widget build(BuildContext context){
-    return Scaffold(
-
-    )
-  }
+  // Widget build(BuildContext context){
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: Text('ShareTodo'),
+  //     ),
+  //     body: ListView.builder(
+  //       itemBuilder: (BuildContext context, int index) {
+  //         return Card(
+  //           child: ListTile(
+  //             title: Text(list[index]),
+  //             onTap: () {
+  //               showDialog(
+  //                 context: context,
+  //                 builder: (_) {
+  //                   return AlertDialog(
+  //                     title: Text(list[index]),
+  //                     actions: <Widget>[
+  //                       FlatButton(
+  //                         child: Text("削除"),
+  //                         onPressed: () => Navigator.pop(context),
+  //                       ),
+  //                       FlatButton(
+  //                         child: Text("閉じる"),
+  //                         onPressed: () => Navigator.pop(context),
+  //                       ),
+  //                     ],
+  //                   );
+  //                 }
+  //               );
+  //             },
+  //           ),
+  //         );
+  //       },
+  //       itemCount: list.length,
+  //     ),
+  //   );
+  // }
 }
